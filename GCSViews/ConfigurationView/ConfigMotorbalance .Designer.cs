8504e39453b39bc4f875849c0cd23ba323b27fa2
王继成 <953637022@qq.com>
2016-07-28 09:44:26 +0800

    动平衡

---
+++ GCSViews/ConfigurationView/ConfigMotorbalance .Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace ByAeroBeHero.GCSViews.ConfigurationView
+{
+    partial class ConfigMotorbalance 
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConfigMotorbalance));
+            this.BtnChenckbalance = new ByAeroBeHero.Controls.MyButton();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblCheckReson = new System.Windows.Forms.Label();
+            this.lblprogress = new System.Windows.Forms.Label();
+            this.lblprogress1 = new System.Windows.Forms.Label();
+            this.lblPositive1 = new System.Windows.Forms.Label();
+            this.lblPositive = new System.Windows.Forms.Label();
+            this.lblNegative1 = new System.Windows.Forms.Label();
+            this.lblNegative = new System.Windows.Forms.Label();
+            this.lblDiff = new System.Windows.Forms.Label();
+            this.lblDiff1 = new System.Windows.Forms.Label();
+            this.lblCheckReson1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnChenckbalance
+            // 
+            resources.ApplyResources(this.BtnChenckbalance, "BtnChenckbalance");
+            this.BtnChenckbalance.Name = "BtnChenckbalance";
+            this.BtnChenckbalance.UseVisualStyleBackColor = true;
+            this.BtnChenckbalance.Click += new System.EventHandler(this.BtnChenckbalance_Click);
+            // 
+            // progressBar
+            // 
+            resources.ApplyResources(this.progressBar, "progressBar");
+            this.progressBar.Name = "progressBar";
+            // 
+            // lblCheckReson
+            // 
+            resources.ApplyResources(this.lblCheckReson, "lblCheckReson");
+            this.lblCheckReson.ForeColor = System.Drawing.Color.White;
+            this.lblCheckReson.Name = "lblCheckReson";
+            // 
+            // lblprogress
+            // 
+            resources.ApplyResources(this.lblprogress, "lblprogress");
+            this.lblprogress.ForeColor = System.Drawing.Color.White;
+            this.lblprogress.Name = "lblprogress";
+            // 
+            // lblprogress1
+            // 
+            resources.ApplyResources(this.lblprogress1, "lblprogress1");
+            this.lblprogress1.ForeColor = System.Drawing.Color.White;
+            this.lblprogress1.Name = "lblprogress1";
+            // 
+            // lblPositive1
+            // 
+            resources.ApplyResources(this.lblPositive1, "lblPositive1");
+            this.lblPositive1.ForeColor = System.Drawing.Color.White;
+            this.lblPositive1.Name = "lblPositive1";
+            // 
+            // lblPositive
+            // 
+            resources.ApplyResources(this.lblPositive, "lblPositive");
+            this.lblPositive.ForeColor = System.Drawing.Color.White;
+            this.lblPositive.Name = "lblPositive";
+            // 
+            // lblNegative1
+            // 
+            resources.ApplyResources(this.lblNegative1, "lblNegative1");
+            this.lblNegative1.ForeColor = System.Drawing.Color.White;
+            this.lblNegative1.Name = "lblNegative1";
+            // 
+            // lblNegative
+            // 
+            resources.ApplyResources(this.lblNegative, "lblNegative");
+            this.lblNegative.ForeColor = System.Drawing.Color.White;
+            this.lblNegative.Name = "lblNegative";
+            // 
+            // lblDiff
+            // 
+            resources.ApplyResources(this.lblDiff, "lblDiff");
+            this.lblDiff.ForeColor = System.Drawing.Color.White;
+            this.lblDiff.Name = "lblDiff";
+            // 
+            // lblDiff1
+            // 
+            resources.ApplyResources(this.lblDiff1, "lblDiff1");
+            this.lblDiff1.ForeColor = System.Drawing.Color.White;
+            this.lblDiff1.Name = "lblDiff1";
+            // 
+            // lblCheckReson1
+            // 
+            resources.ApplyResources(this.lblCheckReson1, "lblCheckReson1");
+            this.lblCheckReson1.ForeColor = System.Drawing.Color.White;
+            this.lblCheckReson1.Name = "lblCheckReson1";
+            // 
+            // ConfigMotorbalance
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblCheckReson1);
+            this.Controls.Add(this.lblDiff);
+            this.Controls.Add(this.lblDiff1);
+            this.Controls.Add(this.lblNegative);
+            this.Controls.Add(this.lblNegative1);
+            this.Controls.Add(this.lblPositive);
+            this.Controls.Add(this.lblPositive1);
+            this.Controls.Add(this.lblprogress1);
+            this.Controls.Add(this.lblprogress);
+            this.Controls.Add(this.lblCheckReson);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.BtnChenckbalance);
+            this.Name = "ConfigMotorbalance";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Controls.MyButton BtnChenckbalance;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblCheckReson;
+        private System.Windows.Forms.Label lblprogress;
+        private System.Windows.Forms.Label lblprogress1;
+        private System.Windows.Forms.Label lblPositive1;
+        private System.Windows.Forms.Label lblPositive;
+        private System.Windows.Forms.Label lblNegative1;
+        private System.Windows.Forms.Label lblNegative;
+        private System.Windows.Forms.Label lblDiff;
+        private System.Windows.Forms.Label lblDiff1;
+        private System.Windows.Forms.Label lblCheckReson1;
+
+    }
+}
---
+++ GCSViews/ConfigurationView/ConfigMotorbalance .cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using ByAeroBeHero.Controls;
+using ByAeroBeHero.HIL;
+using System.Threading;
+
+namespace ByAeroBeHero.GCSViews.ConfigurationView
+{
+    public partial class ConfigMotorbalance  : UserControl, IActivate
+    {
+        public ConfigMotorbalance()
+        {
+            InitializeComponent();
+        }
+
+        public void Activate()
+        {
+           
+        }
+
+        DateTime dt;    //记时  
+        private void BtnChenckbalance_Click(object sender, EventArgs e)
+        {
+            if (!MainV2.comPort.BaseStream.IsOpen)
+            {
+                CustomMessageBox.Show("请连接地面站!", "提示");
+                return;
+            }
+
+            if (MainV2.comPort.MAV.cs.mode != "定高" && MainV2.comPort.MAV.cs.mode != "定点") 
+            {
+                CustomMessageBox.Show("请选则定高或定点模式!", "提示");
+                return;
+            }
+
+            if (MainV2.comPort.MAV.cs.roll > 15) 
+            {
+                CustomMessageBox.Show("滚转角度大于15°!", "提示");
+                return;
+            }
+
+            if (MainV2.comPort.MAV.cs.pitch > 15)
+            {
+                CustomMessageBox.Show("俯仰角度大于15°!", "提示");
+                return;
+            }
+            
+            progressBar.Value = 0;
+
+            this.BtnChenckbalance.Text = "检测中...";
+            Thread myThread = new Thread(DoData);
+            myThread.IsBackground = true;
+            myThread.Start(60); //线程开始  
+            dt = DateTime.Now;  //开始记录当前时间 
+        }
+
+        private delegate void DoDataDelegate(object number);  
+        /// <summary>  
+        /// 进行循环  
+        /// </summary>  
+        /// <param name="number"></param>  
+       private void DoData(object number)  
+       {  
+           if (progressBar.InvokeRequired)  
+           {  
+               DoDataDelegate d = DoData;  
+               progressBar.Invoke(d, number);  
+            }  
+           else  
+           {
+
+               float ch1out = 0;
+               float ch2out = 0;
+               float ch3out = 0;
+               float ch4out = 0;
+               float ch5out = 0;
+               float ch6out = 0;
+               float ch7out = 0;
+               float ch8out = 0;
+
+               int a = 0;
+               for (int i = 1; i <= 60; i++)
+               {
+                   ch1out += MainV2.comPort.MAV.cs.ch1out;
+                   ch2out += MainV2.comPort.MAV.cs.ch2out;
+                   ch3out += MainV2.comPort.MAV.cs.ch3out;
+                   ch4out += MainV2.comPort.MAV.cs.ch4out;
+                   ch5out += MainV2.comPort.MAV.cs.ch5out;
+                   ch6out += MainV2.comPort.MAV.cs.ch6out;
+                   ch7out += MainV2.comPort.MAV.cs.ch7out;
+                   ch8out += MainV2.comPort.MAV.cs.ch8out;
+                   a+= 2;
+                   if(a>=100)
+                       progressBar.Value =100;
+                   else
+                       progressBar.Value = a;
+                   lblprogress1.Text = ((double)a / 120 * 100).ToString("f2")+"%";
+                   Application.DoEvents();  
+                   Thread.Sleep(500);
+               }
+
+               int iCopterType = (int)(float)MainV2.comPort.MAV.param["COPTER_TYPE"];
+
+               float Positivedirection = 0;
+               float Negativedirection = 0;
+               int iFType = 0;
+
+               if (iCopterType == 1)
+               {
+                   Positivedirection = ch1out + ch3out;
+                   Negativedirection = ch2out + ch4out;
+                   iFType = 4;
+               }
+               else if (iCopterType == 2)
+               {
+                   Positivedirection = ch1out + ch3out + ch5out;
+                   Negativedirection = ch2out + ch4out + ch6out;
+                   iFType = 6;
+               }
+               else
+               {
+                   Positivedirection = ch1out + ch3out + ch5out + ch7out;
+                   Negativedirection = ch2out + ch4out + ch6out + ch8out;
+                   iFType = 8;
+               };
+
+               float fPositivedirection = (Positivedirection / (iFType / 2))/60;
+               float fNegativedirection = (Negativedirection / (iFType / 2))/60;
+
+               float iDifference = (Positivedirection - Negativedirection) / (iFType / 2);
+
+               this.lblPositive.Text = fPositivedirection.ToString("f2");
+               this.lblNegative.Text = fNegativedirection.ToString("f2");
+               this.lblDiff.Text = iDifference.ToString("f2");
+
+               string strs = "";
+               if (iDifference > 50)
+               {
+                   strs += "检测原因：飞机航向存在偏差，需要将电机逆时针旋转（从飞机侧面正对电机），提供顺时针的补偿力矩";
+               }
+               else if (iDifference < -50)
+               {
+                   strs += "飞机航向存在偏差，需要将电机顺时针旋转（从飞机侧面正对电机），提供逆时针的补偿力矩";
+               }
+               else
+               {
+                   strs += "飞机航向正常，不需要补偿";
+               }
+
+               this.lblCheckReson1.Text = strs;
+
+               this.BtnChenckbalance.Text = "检测完成";
+            }  
+        }  
+    }
+}
